e37823cff57ad248c9459154266eb7fe482c2175
Kylych Sydykov <kylycsydykov@gmail.com>
2021-01-11 01:51:52 +0600

    calc problem

--- src/us/peaksoft/homeWorkCalculater/models/Main.java
+++ src/us/peaksoft/homeWorkCalculater/models/Main.java
@@ -12,5 +12,29 @@ public class Main {
         RomeCalc romeCalc = new RomeCalcExtends();
         romeCalc.calcRome();
 
+
+//        System.out.println(toRoman(5));
+
+    }
+
+
+    private static String toRoman(int n) {
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "X", "IX", "V", "I"};
+        int[] romanNumeralNums = {1000, 900, 500, 400, 100, 90, 50, 10, 9, 5, 1};
+        String finalRomanNum = "";
+
+        for (int i = 0; i < romanNumeralNums.length; i++) {
+            int currentNum = n / romanNumeralNums[i];
+            if (currentNum == 0) {
+                continue;
+            }
+
+            for (int j = 0; j < currentNum; j++) {
+                finalRomanNum += romanNumerals[i];
+            }
+
+            n = n % romanNumeralNums[i];
+        }
+        return finalRomanNum;
     }
 }
--- src/us/peaksoft/homeWorkCalculater/models/RomeCalcExtends.java
+++ src/us/peaksoft/homeWorkCalculater/models/RomeCalcExtends.java
@@ -11,22 +11,23 @@ public class RomeCalcExtends extends RomeCalc {
 
     @Override
     public String calcRome() {
-
         System.out.println("ENTER ROME NUMBER: ");
-
         String a, b;
-        char operation;
-        String result = null;
-        convertToArabNumbers(String.valueOf(romeNumbers));
-        convertToRomeNumbers(arabNumbers);
-        a = scanner.nextLine();
+        char operation = 0;
+        String result;
+
+        a = scanner.next();
         operation = scanner.next().charAt(0);
-        b = scanner.nextLine();
+        b = scanner.next();
         a = a.toUpperCase();
         b = b.toUpperCase();
+        convertToArabNumbers(a);
+        convertToArabNumbers(b);
+//        convertToRomeNumbers(Integer.parseInt(a));
+//        convertToRomeNumbers(Integer.parseInt(b));
 
-
-        getResOfRomNum(operation);
+        result = a + b;
+        System.out.println(result);
 
 
         return result;
@@ -34,96 +35,75 @@ public class RomeCalcExtends extends RomeCalc {
 
 
     public int convertToArabNumbers(String num) {
-        int resC = 0;
-        if (num.equals(romeNumbers[0]))
-            resC = 1;
-        else if (num.equals(romeNumbers[1]))
-            resC = 2;
-        else if (num.equals(romeNumbers[2]))
-            resC = 3;
-        else if (num.equals(romeNumbers[3]))
-            resC = 4;
-        else if (num.equals(romeNumbers[4]))
-            resC = 5;
-        else if (num.equals(romeNumbers[5]))
-            resC = 6;
-        else if (num.equals(romeNumbers[6]))
-            resC = 7;
-        else if (num.equals(romeNumbers[7]))
-            resC = 8;
-        else if (num.equals(romeNumbers[8]))
-            resC = 9;
-        else if (num.equals(romeNumbers[9]))
-            resC = 10;
-        return resC;
+        int resArabNumbers = 0;
+        if (num.equals(romeNumbers[0])) {
+            resArabNumbers = 1;
+        } else if (num.equals(romeNumbers[1])) {
+            resArabNumbers = 2;
+        } else if (num.equals(romeNumbers[2])) {
+            resArabNumbers = 3;
+        } else if (num.equals(romeNumbers[3])) {
+            resArabNumbers = 4;
+        } else if (num.equals(romeNumbers[4])) {
+            resArabNumbers = 5;
+        } else if (num.equals(romeNumbers[5])) {
+            resArabNumbers = 6;
+        } else if (num.equals(romeNumbers[6])) {
+            resArabNumbers = 7;
+        } else if (num.equals(romeNumbers[7])) {
+            resArabNumbers = 8;
+        } else if (num.equals(romeNumbers[8])) {
+            resArabNumbers = 9;
+        } else if (num.equals(romeNumbers[9])) {
+            resArabNumbers = 10;
+        }
+        return resArabNumbers;
     }
 
-    public String convertToRomeNumbers(int... numF) {
-
-        String conStr = null;
-
-        for (int i = 0; i < numF.length; i++) {
-            if (numF[i] == 1) {
-                conStr = romeNumbers[0];
-            } else if (numF[i] == 2) {
-                conStr = romeNumbers[1];
-            } else if (numF[i] == 3) {
-                conStr = romeNumbers[2];
-            } else if (numF[i] == 4) {
-                conStr = romeNumbers[3];
-            } else if (numF[i] == 5) {
-                conStr = romeNumbers[4];
-            } else if (numF[i] == 6) {
-                conStr = romeNumbers[5];
-            } else if (numF[i] == 7) {
-                conStr = romeNumbers[6];
-            } else if (numF[i] == 8) {
-                conStr = romeNumbers[7];
-            } else if (numF[i] == 9) {
-                conStr = romeNumbers[8];
-            } else if (numF[i] == 10) {
-                conStr = romeNumbers[9];
-            } else {
-                conStr = "";
-            }
+    public String convertToRomeNumbers(int num) {
+        String conStr = "";
+        if (num == 1) {
+            conStr = romeNumbers[0];
+        } else if (num == 2) {
+            conStr = romeNumbers[1];
+        } else if (num == 3) {
+            conStr = romeNumbers[2];
+        } else if (num == 4) {
+            conStr = romeNumbers[3];
+        } else if (num == 5) {
+            conStr = romeNumbers[4];
+        } else if (num == 6) {
+            conStr = romeNumbers[5];
+        } else if (num == 7) {
+            conStr = romeNumbers[6];
+        } else if (num == 8) {
+            conStr = romeNumbers[7];
+        } else if (num == 9) {
+            conStr = romeNumbers[8];
+        } else if (num == 10) {
+            conStr = romeNumbers[9];
         }
         return conStr;
     }
 
-    public String getResOfRomNum(char operation) {
-        String resS = "";
-        switch (operation) {
-            case '+':
-                resS = romeNumbers[9];
-                break;
-            case 2:
-                resS = romeNumbers[9] + romeNumbers[9];
-                break;
-            case 3:
-                resS = romeNumbers[9] + romeNumbers[9] + romeNumbers[9];
-                break;
-            case 4:
-                resS = romeNumbers[9] + "L";
-                break;
-            case 5:
-                resS = "L";
-                break;
-            case 6:
-                resS = "L" + romeNumbers[9];
-                break;
-            case 7:
-                resS = "L" + romeNumbers[9] + romeNumbers[9];
-                break;
-            case 8:
-                resS = "L" + romeNumbers[9] + romeNumbers[9] + romeNumbers[9];
-                break;
-            case 9:
-                resS = romeNumbers[9] + "C";
-            case 10:
-                resS = "C";
-        }
+//    public String getResOfRomNum(char operation) {
+//        String resS = "";
+//        switch (operation) {
+//            case '+':
+//                resS = arabNumbers[0] + romeNumbers[0];
+//                break;
+//            case '-':
+//                resS = arabNumbers[1] - romeNumbers[1];
+//                break;
+//            case '/':
+//                resS = arabNumbers[0] / romeNumbers[0];
+//                break;
+//            case '*':
+//                resS = arabNumbers[0] * romeNumbers[0];
+//                break;
+//        }
 //        resS += convertToRomeNumbers(operation);
-
-        return resS;
-    }
+//
+//        return resS;
+//    }
 }

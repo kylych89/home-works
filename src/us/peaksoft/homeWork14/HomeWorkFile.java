10b9c14e032a281b51684916068015814d0cd5cc
Kylych Sydykov <kylycsydykov@gmail.com>
2021-01-18 17:36:53 +0600

    finished home work 14.1 and 14.2

--- src/us/peaksoft/homeWork14/HomeWorkFile.java
+++ src/us/peaksoft/homeWork14/HomeWorkFile.java
@@ -15,42 +15,7 @@ public class HomeWorkFile {
                 System.out.println("No");
             }
             String message = "A a\nB b\nC c\nD d\nE e\nF f\nG g\nH h\nI i\nJ j\nK k\nL l\nM m\nN n\nO o\nP p\nQ q\nR r\nS s\nT t\nU u\nV v\nW w\nX x\nY y\nZ z" +
-                    "\n1 one\n" +
-                    "2 two\n" +
-                    "3 three\n" +
-                    "4 four\n" +
-                    "5 five\n" +
-                    "6 six\n" +
-                    "7 seven\n" +
-                    "8 eight\n" +
-                    "9 nine\n" +
-                    "10 ten\n" +
-                    "11 eleven\n" +
-                    "12 twelve\n" +
-                    "13 thirteen\n" +
-                    "14 fourteen\n" +
-                    "15 fifteen\n" +
-                    "16 sixteen\n" +
-                    "17 seventeen\n" +
-                    "18 eighteen\n" +
-                    "19 nineteen\n" +
-                    "20 twenty\n" +
-                    "21 twenty-one\n" +
-                    "22 twenty-two\n" +
-                    "23 twenty-three\n" +
-                    "24 twenty-four\n" +
-                    "25 twenty-five\n" +
-                    "26 twenty-six\n" +
-                    "27 twenty-seven\n" +
-                    "28 twenty-eight\n" +
-                    "29 twenty-nine\n" +
-                    "30 thirty\n" +
-                    "31 thirty-one\n" +
-                    "32 thirty-two\n" +
-                    "33 thirty-three\n" +
-                    "34 thirty-four\n" +
-                    "35 thirty-five\n" +
-                    "36 thirty-six";
+                    "\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n";
             byte[] bytes = message.getBytes();
             fileOutputStream.write(bytes);
             fileOutputStream.flush();

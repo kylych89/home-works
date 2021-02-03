b29e809ff2d40b502b146099203e51b46d0d1375
Kylych Sydykov <kylycsydykov@gmail.com>
2021-02-03 17:45:24 +0600

    home work 16 is finished

--- src/us/peaksoft/homeWork16/Main.java
+++ src/us/peaksoft/homeWork16/Main.java
@@ -2,6 +2,7 @@ package us.peaksoft.homeWork16;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Alsalam on 01.02.2021.
@@ -12,8 +13,29 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
 
-        Integer maappedBy = list.stream()
-                .reduce(0, (x, y) -> x + y / 2);
-        System.out.println(maappedBy);
+        // листтин элементтердин суммасын таап листти чыгаруу
+        Integer findSumOfList = list.stream()
+                .reduce(0, (x, y) -> x + y);
+        System.out.println("Sum found is-> " + findSumOfList);
+
+        // орточо арифметикалык санын таап листти чыгаруу
+        Double average = list.stream()
+                .mapToDouble(x -> x)
+                .average()
+                .getAsDouble();
+        System.out.println("Average found is-> " + average);
+
+
+        // канча жолу кайталанган листти чыгаруу
+        Long count = list.stream()
+                .count();
+        System.out.println("Count-> " + count);
+
+        // сорттолгон листти чыгаруу
+        List<Integer> sortedList= list.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(sortedList);
+
     }
 }
